7a72c46f46954c9af40e9260c5266b2bb6a10468
Marcin Gnap <258953@student.pwr.edu.pl>
2021-11-24 01:15:38 +0100

    Basic commit v10

--- src/tb/soft/BetterPerson.java
+++ src/tb/soft/BetterPerson.java
@@ -5,4 +5,11 @@ public class BetterPerson extends NewPerson{
     public BetterPerson(String first_name, String last_name, String job_name) throws PersonException {
         super(first_name, last_name, job_name);
     }
+
+
+
+
+
+
+
 }
